a2f34ab7506a0acac72a2fc2e8ea995e87502bef
David Auinger <post@davidauinger.eu>
2023-06-15 18:46:57 +0200

    Support parallel sorting for directories, added install routines

--- qbfsortdir.sh
+++ qbfsortdir.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+#
+# usage: qbfsortdir.sh [-j parallelJobs|-n] inputFile outputFile [options]
+#
+parallelJobs=1
+while getopts "j:n" option
+do
+  case "${option}" in
+    j) parallelJobs=$OPTARG;;
+    n) parallelJobs=$(nproc);;
+  esac
+done
+shift $((${OPTIND} - 1))
 inputDir=$1
 outputDir=$2
 shift 2
+
+mkfifo fifo-$$
+exec 3<>fifo-$$
+rm fifo-$$
+for ((i=0; i<${parallelJobs}; ++i))
+do
+  printf "." >&3
+done
+
 mkdir --parents "${outputDir}"
 for f in $(ls "${inputDir}")
 do
-  ./qbfsort --input "${inputDir}"/"$f" --output "${outputDir}"/$(basename "$f") $@
+  read -u 3 -n 1 x
+  (
+    ./qbfsort --input "${inputDir}"/"$f" --output "${outputDir}"/$(basename "$f") $@
+    printf "." >&3
+  )&
 done
+
+wait
